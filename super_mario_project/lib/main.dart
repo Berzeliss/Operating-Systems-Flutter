d6d8a3eeefdec8bb9d1caaa779475e436e4905db
tinwan.ng <tinwan.ng@govmind.tech>
2025-02-06 17:57:31 +0100

    initial commit

---
+++ super_mario_project/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:super_mario_project/homepage.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      );
+  }
+}
+
